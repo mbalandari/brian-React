f32c338f620b4f078e60e7ab2e33b17c8d9ad6db
mbalandari <m.balandari.toroghi@gmail.com>
2021-03-30 15:43:50 +0300

    3rd Tue

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import pet, { ANIMALS } from '@frontendmasters/pet';
+import Results from './Results';
 import useDropdown from './useDropdown';
 
 
@@ -18,7 +19,6 @@ const SearchParams = () => {
         });
 
         setPets(animals || []);
-
     }
 
     useEffect(() => {
@@ -49,6 +49,7 @@ const SearchParams = () => {
                 <BreedDropdown />
                 <button>Submit</button>
             </form>
+            <Results pets={pets} />
         </div>
     );
 };
